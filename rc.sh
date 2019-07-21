2290e9d6300c69b252f37b8559610deba408c57b
Mateusz Jończyk <mat.jonczyk@o2.pl>
2019-07-21 10:38:59 +0200

    Move mouse keys enablement to rc.sh
    It does not work in xsessionrc, probably because GNOME resets the
    settings.

--- rc.sh
+++ rc.sh
@@ -10,6 +10,10 @@ my_clear_flash
 
 rfkill block bluetooth
 
+# Enable mouse keys (moving the mouse cursor by pressing keys on the numpad):
+xkbset mousekeys
+# and set its speed - modified settings from https://askubuntu.com/a/499037
+xkbset mousekeysaccel 60 10 20 20 3
 
 my_date=$(date "+%Y-%m-%d_%H-%M-%S")
 cp -a .bash_history .bash_history_$my_date
